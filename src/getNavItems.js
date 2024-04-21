871966887ffb96b075c21319ca595232e576d456
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-04-21 18:35:23 +0300

    Add getNavItems func

---
+++ src/getNavItems.js
@@ -0,0 +1,42 @@
+export function getNavItems() {
+
+    const items = [
+        {
+            img: "/img/sidebar/nav/user.svg",
+            url: "#",
+            title: "Профиль"
+        },
+        {
+            img: "/img/sidebar/nav/home.svg",
+            url: "#",
+            title: "Главная кабинета"
+        },
+        {
+            img: "/img/sidebar/nav/notification.svg",
+            url: "#",
+            title: "Уведомления"
+        },
+        {
+            img: "/img/sidebar/nav/calendar.svg",
+            url: "#",
+            title: "Записи на приём"
+        },
+        {
+            img: "/img/sidebar/nav/card.svg",
+            url: "#",
+            title: "Медкарта"
+        },
+        {
+            img: "/img/sidebar/nav/diary.svg",
+            url: "#",
+            title: "Дневник здоровья"
+        },
+        {
+            img: "/img/sidebar/nav/exit.svg",
+            url: "#",
+            title: "Выйти"
+        }
+    ];
+
+  return items;
+}
