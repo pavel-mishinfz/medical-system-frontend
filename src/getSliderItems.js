3152d4dd2675f7263e51063773ee34bb89c0fd97
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-04-21 18:35:55 +0300

    Add getSliderItems func

---
+++ src/getSliderItems.js
@@ -0,0 +1,27 @@
+export function getSliderItems() {
+
+    const items = [
+        {
+            url: '/img/slider/slide1.png', 
+            title: 'Запись на прием', 
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto et eaque culpa ad unde distinctio cumque sed iste voluptatem quod veniam magnam, repellendus exercitationem, iusto ipsa laudantium aspernatur cupiditate? A facere quam maiores et laboriosam, qui sapiente cupiditate harum velit!'
+        },
+        {
+            url: '/img/slider/slide1.png', 
+            title: 'Запись на прием', 
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto et eaque culpa ad unde distinctio cumque sed iste voluptatem quod veniam magnam, repellendus exercitationem, iusto ipsa laudantium aspernatur cupiditate? A facere quam maiores et laboriosam, qui sapiente cupiditate harum velit!'
+        },
+        {
+            url: '/img/slider/slide1.png', 
+            title: 'Запись на прием', 
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto et eaque culpa ad unde distinctio cumque sed iste voluptatem quod veniam magnam, repellendus exercitationem, iusto ipsa laudantium aspernatur cupiditate? A facere quam maiores et laboriosam, qui sapiente cupiditate harum velit!'
+        },
+        {
+            url: '/img/slider/slide1.png', 
+            title: 'Запись на прием', 
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto et eaque culpa ad unde distinctio cumque sed iste voluptatem quod veniam magnam, repellendus exercitationem, iusto ipsa laudantium aspernatur cupiditate? A facere quam maiores et laboriosam, qui sapiente cupiditate harum velit!'
+        }
+    ];
+
+  return items;
+}
