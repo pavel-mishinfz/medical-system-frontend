1cb4ada78aafc176ebba0cf94652f378366a3302
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-04-22 21:31:37 +0300

    Add navigation on specializations

--- src/components/Specialization/SpezializationItem.jsx
+++ src/components/Specialization/SpezializationItem.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import SpezializationImage from './SpezializationImage';
 import SpezializationTitle from './SpezializationTitle';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function SpezializationItem({ data: { id, name, img } }) {
+  const navigate = useNavigate();
+  
   return (
-    <div className="direct__list-item">
+    <div className="direct__list-item" onClick={() => navigate(`/specializations/${id}`)}>
       <SpezializationImage src={img} />
       <SpezializationTitle title={name} />
     </div>
