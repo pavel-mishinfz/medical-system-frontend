c2829ac3e36fef4d240c7c79d40a07bfb8f03968
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-04-22 21:33:45 +0300

    Fix output of doctor's experience

--- src/components/Doctor/DoctorInfo.jsx
+++ src/components/Doctor/DoctorInfo.jsx
@@ -5,7 +5,19 @@ export default function DoctorInfo({data: {name, surname, patronymic, desc, exp}
     <div className="doctor__info">
         <h3 className="doctor__info-name">{`${surname} ${name} ${patronymic}`}</h3>
         <p className="doctor__info-spec">{desc}</p>
-        <p className="doctor__info-exp">Стаж {exp} лет</p>
+        <p className="doctor__info-exp">Стаж {exp} {formatExperience(exp)}</p>
     </div>
   );
+}
+
+function formatExperience(exp) {
+  if ((exp > 9 && exp < 15) || exp % 10 >= 5) {
+    return 'лет';
+  }
+  if (exp % 10 === 1) {
+    return 'год';
+  }
+  if ((exp % 10 > 1) && (exp % 10 < 5)) {
+    return 'года';
+  }
 }
