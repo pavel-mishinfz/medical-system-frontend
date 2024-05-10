a3a6136b4e5b6a7c7630e51b7c2d83b5521b7368
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-05-10 18:29:29 +0300

    Add ForgotPassword page

---
+++ src/components/ForgotPassword.jsx
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import Card from './Card/Card';
+import Input from './Form/Input';
+
+
+const ForgotPassword = () => {
+    const [email, setEmail] = useState('');
+    const [textBtn, setTextBtn] = useState('Сбросить пароль');
+    const [modify, setModify] = useState('');
+
+    const handleRequestResetTokenPassword = async () => {
+        const requestBody = {
+          email: email
+        };
+
+        axios.post('http://' + window.location.hostname + ':8000/auth/forgot-password', requestBody)
+        .then(response => {
+            setTextBtn('Сообщение успешно отправлено!');
+            setModify('btn--success');
+            setTimeout(function() {
+                setTextBtn('Сбросить пароль');
+                setModify('');
+            }, 3000);
+            console.log('Request reset token passwrod successful:', response.status);
+        })
+        .catch(error => {
+            console.error('Request reset token passwrod  failed:', error);
+        });
+    };
+
+    return (
+        <div className="container">
+            <div className="modal">
+                <Card 
+                title={'Забыли пароль?'} 
+                subtitle={'Не волнуйтесь, такое случается. Пожалуйста, введите email, привязанный к вашему аккаунту.'}
+                fields={<Input type={'email'}
+                              htmlFor={'email'}
+                              placeholder={'user@example.com'}
+                              title={'Email'}
+                              value={email}
+                              onChangeInput={(e) => setEmail(e.target.value)}
+                        />
+                }
+                modify={modify}
+                textBtn={textBtn}
+                handleClick={handleRequestResetTokenPassword}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ForgotPassword;
