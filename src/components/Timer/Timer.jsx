9a7598c34c7b16a7184de9b76dd34c831e9ebf33
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-05-10 18:34:10 +0300

    Add Timer component to count validity time of confirmation code

---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,68 @@
+import React, {useLayoutEffect, useRef, useState} from 'react';
+
+
+const resendTimeout = 180000;
+
+const Timer = ({handle, disabledInput}) => {
+
+    const [sendAt, setSendAt] = useState(Date.now());
+    const now = useNow(1000, sendAt, (now) => {
+        if (sendAt && resendTimeout - (now - sendAt) < 0) {
+            setSendAt();
+        }
+    });
+
+    const msToRerend = sendAt ? resendTimeout - (now - sendAt) : 0;
+    const isDisabled = msToRerend > 0;
+
+    const handleSend = () => {
+        handle();
+        setSendAt(Date.now());
+    }
+
+    return (
+        <button type="button" disabled={isDisabled} onClick={handleSend}>
+            {isDisabled ? disabledInput(false) : disabledInput(true)}
+            {isDisabled ? `${timeToRerend(msToRerend)}` : 'Отправить снова' }
+        </button>
+    );
+};
+
+export default Timer;
+
+
+function timeToRerend(msToRerend) {
+    let allSeconds = Math.floor(msToRerend / 1000);
+    let minutes = Math.floor(allSeconds / 60);
+    let seconds = allSeconds - minutes*60;
+
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`
+}
+
+
+function useNow(updateInterval, enabled, cb) {
+    const cbRef = useRef(cb);
+    cbRef.current = cb;
+    const [now, setNow] = useState(Date.now());
+
+    useLayoutEffect(() => {
+        if (!enabled) {
+            return;
+        }
+
+        setNow(Date.now());
+        cbRef.current?.(Date.now());
+
+        const interval = setInterval(() => {
+            setNow(Date.now());
+            cbRef.current?.(Date.now());
+        }, updateInterval);
+
+        return () => {
+            clearInterval(interval);
+        };
+
+    }, [updateInterval, enabled]);
+
+    return now;
+}
