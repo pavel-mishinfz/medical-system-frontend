55a007967ff9a9b5a800da97f228684002c6c08c
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-05-10 18:40:59 +0300

    Add new routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import MedicalCard from './components/MedicalCard';
 import MedicalCardPagesList from './components/MedicalCardPagesList';
 import HealthDiary from './components/HealthDiary';
 import Templates from './components/Templates';
+import Login from './components/Login';
+import Register from './components/Register';
+import ConfirmRegister from './components/ConfirmRegister';
+import ForgotPassword from './components/ForgotPassword';
+import ResetPassword from './components/ResetPassword';
+import Confirm from './components/Confirm';
+import Profile from './components/Profile';
+import ChangeEmail from './components/ChangeEmail';
 
 
 function App() {
@@ -12,6 +20,14 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/confirm-register" element={<ConfirmRegister />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/confirm" element={<Confirm />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/change-email" element={<ChangeEmail />} />
         <Route path="/specializations/:id" element={<Specialization />} />
         <Route path="/medical-card" element={<MedicalCard />} />
         <Route path="/medical-card/:id/pages" element={<MedicalCardPagesList />} />
