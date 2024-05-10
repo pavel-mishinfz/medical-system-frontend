bbd70d1aac90370be678fe72aea8246c38fc566f
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-05-10 18:21:36 +0300

    Update structure of From component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import FormHeader from './FormHeader';
 import InputList from './InputList';
 import Button from '../Button/Button';
-import SmallTextRegister from './SmallTextRegister';
+import SmallText from './SmallText';
 
 
-export default function Form({title, inputListData, textBtn, onHandleBtn, onCloseModal}) {
+export default function Form({isLogin, title, inputListData, textBtn, onHandleBtn, onCloseModal, smallText}) {
 
   return (
-    <div class="form">
+    <div className="form">
         {onCloseModal && <div className="form__close" onClick={onCloseModal}>&times;</div>}
         <FormHeader title={title} />
         <InputList inputListData={inputListData} />
+        {isLogin && (<SmallText modify={'form__small--login'} nav={{addr: '/forgot-password', addrText: 'Забыли пароль?'}}/>)}
         <Button text={textBtn} onHandleClick={onHandleBtn}/>
-        <SmallTextRegister />
+        <SmallText text={smallText.text} nav={smallText.nav}/>
     </div>
   );
 }
