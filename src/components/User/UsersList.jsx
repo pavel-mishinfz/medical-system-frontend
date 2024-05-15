35a07e4c1ca3cdfdbfe149beb880166932f38218
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-05-16 00:40:38 +0300

    Add links to user's medical card and health diary

--- src/components/User/UsersList.jsx
+++ src/components/User/UsersList.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import RecordPanel from '../RecordPanel/RecordPanel';
 import User from './User';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function UsersList({users, openRegisterModal}) {
+  const navigate = useNavigate();
+
   return (
     <div className="spec__doctors">
         {users.map(user => 
         <div key={user.id} className="spec__doctors-item">
           <User userData={user}/>
-          {user.specialization_id !== null && (
+          {user.specialization_id !== null ? (
             <RecordPanel doctorId={user.id} openRegisterModal={openRegisterModal}/>
+          ) : (
+            <div className="panel-user">
+              <div className="panel-user__item" onClick={() => navigate(`/medical-card/${user.id}`)}>
+                <img src="/img/sidebar/nav/card.svg" alt="medcard" />
+                <p>Медкарта</p>
+              </div>
+              <div className="panel-user__item" onClick={() => navigate(`/health-diary/${user.id}`)}>
+                <img src="/img/sidebar/nav/diary.svg" alt="diary" />
+                <p>Дневник здоровья</p>
+              </div>
+            </div>
           )}
         </div>
         )}
