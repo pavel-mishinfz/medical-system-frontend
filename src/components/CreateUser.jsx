385cc4079ebd66e1e206d91c2227275e3c830614
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-05-16 00:39:13 +0300

    Refactore code

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,35 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import Sidebar from './Sidebar/Sidebar';
 import Head from './Head/Head';
-import UsersList from './User/UsersList';
 import Header from './Header/Header';
-import { useNavigate } from 'react-router-dom';
 import CreateAccount from './Form/CreateAccount';
 
 
 const CreateUser = () => {
-  const navigate = useNavigate();
-  const [users, setUsers] = useState([]);
   const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
   const [userData, setUserData] = useState(null);
-  const [filter, setFilter] = useState(0);
   const [isDoctor, setIsDoctor] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://'+ window.location.hostname + `:8000/users`);
-    
-        setUsers(response.data.filter(user => user.is_superuser === false));
-
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    
-    fetchData();
-  }, [])
 
 
   return (
