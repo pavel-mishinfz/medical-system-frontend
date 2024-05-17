c21cfd8444042d9dcf6eedf1456a10f9752eeada
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-05-17 15:55:18 +0300

    Add option for user verification

--- src/components/Options/OptionItem.jsx
+++ src/components/Options/OptionItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 
-export default function OptionItem({ data: {src, text, onHandleClick} }) {
+export default function OptionItem({ data: {src, text, isVefiry, onHandleClick} }) {
   return (
-    <div className="page__options-item" onClick={onHandleClick}>
+    <div className="page__options-item" style={isVefiry ? {opacity: 0.3, pointerEvents: 'none'} : {}} onClick={onHandleClick}>
         <img src={src} alt="edit card" />
         <p>{text}</p>
     </div>
