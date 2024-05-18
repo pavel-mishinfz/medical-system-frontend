7ec033ca6256d231337c314e88c0e7127819ed27
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-05-18 16:27:01 +0300

    Add Alert component to MedicalCard

--- src/components/MedicalCard.jsx
+++ src/components/MedicalCard.jsx
@@ -8,6 +8,7 @@ import Button from './Button/Button';
 import { useNavigate, useParams } from 'react-router-dom';
 import Header from './Header/Header';
 import { getNewMedicalCard } from '../getNewMedicalCard';
+import Alert from './Alert/Alert';
 
 
 const MedicalCard = ({currentUserData}) => {
@@ -136,15 +137,12 @@ const MedicalCard = ({currentUserData}) => {
                         <Head
                             setSidebarIsOpen={setSidebarIsOpen}
                             isAuthenticated
-                        // setUserData={(data) => setUserData(data)}
                         />
                         <section className="section">
                             <div className="medcard">
                                 <Header title={'Электронная медицинская карта'} />
                                 {!hasMedcard && cardIsLoading && !currentUserData.is_superuser && (
-                                    <div className='alert' style={{ padding: '20px', maxWidth: '900px', minWidth: '290px', border: '3px solid #1a5dd0', backgroundColor: '#d3e6ff' }}>
-                                        <p className='alert__text' style={{ color: '#111', fontWeight: '500', fontSize: '1.6rem', lineHeight: 1.5 }}>Уважаемый пользователь! У Вас еще нет электронной медицинской карты. Вы можете завести её, если лично посетите наше учреждение. В случае отсутствия данной возможности, Вы всегда можете отправить <a href="#" style={{ color: '#1a5dd0' }}>необходимые документы</a> на почту <a href="#" style={{ color: '#1a5dd0' }}>admin@admin.ru</a></p>
-                                    </div>
+                                    <Alert />
                                 )}
                                 {!hasMedcard && !addMedcard && cardIsLoading && currentUserData.is_superuser && (
                                     <Button
