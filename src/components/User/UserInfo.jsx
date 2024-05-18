e323eba073c21c42a21dd661f543757689dc9929
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-05-18 16:26:09 +0300

    Add email field for user's profile

--- src/components/User/UserInfo.jsx
+++ src/components/User/UserInfo.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import moment from 'moment';
+import { getAgeFromBirthDate, getTextAge } from '../Profile/ProfileInfo';
 
 
 export default function UserInfo({data}) {
   const experience = data.date_employment && calculateExperience(data.date_employment);
+  const age = data.birthday && (getAgeFromBirthDate(data.birthday));
 
   return (
     <div className="doctor__info">
@@ -15,7 +17,8 @@ export default function UserInfo({data}) {
         </>
         :
         <>
-        <p className="doctor__info-exp">Дата рождения {moment(data.birthday).utc(true).format('DD/MM/YYYY')}</p>
+        <p className="doctor__info-exp">{age} {getTextAge(age)}</p>
+        <p className="doctor__info-desc">{data.email}</p>
         </>
         }
     </div>
@@ -25,7 +28,7 @@ export default function UserInfo({data}) {
 export const formatExperience = (exp) => {
   if (exp % 10 === 1 && exp % 100 !== 11) {
     return 'год';
-  } else if ([2, 3, 4].includes(exp % 10) && ![12, 13, 14].includes(exp % 100)) {
+  } else if (([2, 3, 4].includes(exp % 10) && ![12, 13, 14].includes(exp % 100)) || exp < 0) {
     return 'года';
   } else {
     return 'лет';
